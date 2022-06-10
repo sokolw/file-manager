fd107d857da0a12f074e3c6fc78a57afdc923525
sokolw <sokolw21@gmail.com>
2022-06-10 07:27:09 +0300

    feat: implement up

--- src/action-functions/nwd/upDirectory.mjs
+++ src/action-functions/nwd/upDirectory.mjs
@@ -1,3 +1,13 @@
+import { Application } from './../../app.js';
+import path from 'path';
+
 export const upDirectory = (args) => {
-  return 'upDirectory';
+  const arrPath = Application._currentPath
+    .split(path.sep);
+  if (arrPath.length > 1) {
+    arrPath.pop();
+    Application._currentPath = path.join(...arrPath, arrPath.length === 1 ? path.sep : '');
+  }
+  
+  return '';
 }
