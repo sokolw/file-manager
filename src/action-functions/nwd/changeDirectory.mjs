116e6e4d7376493b57768830c2f3aabec0936980
sokolw <sokolw21@gmail.com>
2022-06-11 07:52:22 +0300

    feat: implement cd

--- src/action-functions/nwd/changeDirectory.mjs
+++ src/action-functions/nwd/changeDirectory.mjs
@@ -1,3 +1,24 @@
-export const changeDirectory = (args) => {
+import { messages } from "../../enums/messages.mjs";
+import { cliArgsValidator } from "../../validators/cliArgsValidator.mjs"
+import { EOL } from 'os';
+import { isExistDir } from "../system/paths.mjs";
+import { Application } from "../../app.js";
 
+const argsCount = 1;
+
+export const changeDirectory = async (args) => {
+  const space = ' ';
+  const spacedPath = [args.join(space)];
+  if (cliArgsValidator(spacedPath, argsCount)){
+    const pathToDir = await isExistDir(...spacedPath);
+
+    if (pathToDir !== null) {
+      Application._currentPath = pathToDir;
+    } else {
+      return messages.fail().concat(EOL);
+    }
+
+  }
+
+  return '';
 }
