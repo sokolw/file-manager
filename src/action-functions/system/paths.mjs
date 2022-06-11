92efa13c27cca4bbdb8d69c16af3ef424b7e2d91
sokolw <sokolw21@gmail.com>
2022-06-11 07:50:33 +0300

    feat: implement custom paths

--- src/action-functions/system/paths.mjs
+++ src/action-functions/system/paths.mjs
@@ -1,9 +1,55 @@
-import os from 'os';
+import { opendir, access } from 'fs/promises';
+import { constants } from 'fs';
+import os, { EOL } from 'os';
+import path from 'path';
+import { Application } from './../../app.js';
 
-const getDefaultPath = async () => {
+const getDefaultPath = () => {
   return os.homedir();
 }
 
+const isExistDir = async (rawPath) => {
+  const pathToDir = path.isAbsolute(rawPath) ? 
+    rawPath : path.join(Application._currentPath, rawPath);
+
+  try {
+    const directory = await opendir(pathToDir);
+    directory.close();
+  } catch {
+    return null;
+  }
+
+  return pathToDir;
+}
+
+const isExistPathToFile = async (rawPath) => {
+  const pathToDir = await isExistDir(rawPath);
+  if (pathToDir !== null) {
+    return null;
+  }
+
+  const pathToFile = path.isAbsolute(rawPath) ? 
+    rawPath : path.join(Application._currentPath, rawPath);
+
+  try {
+    await access(pathToFile, constants.F_OK);
+    console.log('can access ' + pathToFile);
+  } catch {
+    console.log('cannot access '+ pathToFile);
+    return null;
+  }
+
+  return pathToFile;
+}
+
+const createPath = (rawPath) => {
+  return path.isAbsolute(rawPath) ? 
+  rawPath : path.join(Application._currentPath, rawPath);
+}
+
 export {
-  getDefaultPath
+  getDefaultPath,
+  isExistDir,
+  isExistPathToFile,
+  createPath
 }
