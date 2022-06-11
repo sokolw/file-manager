03d5bbe64e12ff4bacd844379b30663374bbaaea
sokolw <sokolw21@gmail.com>
2022-06-11 07:53:49 +0300

    feat: implement add

--- src/action-functions/file-system/createEmptyFile.mjs
+++ src/action-functions/file-system/createEmptyFile.mjs
@@ -1,3 +1,18 @@
-export const createEmptyFile = (args) => {
+import { open } from 'fs/promises';
+import { EOL } from 'os';
+import { messages } from '../../enums/messages.mjs';
+import { createPath } from '../system/paths.mjs';
 
+export const createEmptyFile = async (args) => {
+  const space = ' ';
+  const spacedPath = args.join(space);
+  const pathToFile = createPath(spacedPath);
+  try {
+    const emptyFile = await open(pathToFile, 'wx+');
+    await emptyFile.close();
+  } catch {
+    return messages.fail().concat(EOL);
+  }
+
+  return '';
 }
