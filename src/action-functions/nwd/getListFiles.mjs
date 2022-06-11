75450fd7a219b2182d4e1c52b897b62274af7a6e
sokolw <sokolw21@gmail.com>
2022-06-11 07:53:08 +0300

    feat: implement ls

--- src/action-functions/nwd/getListFiles.mjs
+++ src/action-functions/nwd/getListFiles.mjs
@@ -1,3 +1,31 @@
-export const getListFiles = (args) => {
+import { readdir } from 'fs/promises';
+import { messages } from '../../enums/messages.mjs';
+import { EOL } from 'os';
+import { cliArgsValidator } from '../../validators/cliArgsValidator.mjs';
+import { Application } from '../../app.js';
 
+const argsCount = 0;
+
+export const getListFiles = async (args) => {
+  const listFiles = {};
+  if (cliArgsValidator(args, argsCount)){
+    const currentPath = Application._currentPath;
+    try {
+      listFiles.result = (await readdir(currentPath, { withFileTypes : true }))
+        .map(item => {
+          if (item.isDirectory()){
+            return `<dir>\t${item.name}`;
+          }
+          return `<file>\t${item.name}`;
+        })
+        .join(EOL)
+        .concat(EOL);
+    } catch {
+      return messages.fail().concat(EOL);
+    }
+  } else {
+    return messages.invalid().concat(EOL);
+  }
+
+  return `${messages.lineDelimiter}${EOL}${listFiles.result}${messages.lineDelimiter}${EOL}`;
 }
