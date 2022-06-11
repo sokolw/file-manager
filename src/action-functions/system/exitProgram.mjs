c55a8667b45cfab2ef9007355dc97e39d11886cf
sokolw <sokolw21@gmail.com>
2022-06-11 23:04:43 +0300

    feat: implement exit

--- src/action-functions/system/exitProgram.mjs
+++ src/action-functions/system/exitProgram.mjs
@@ -1,3 +1,19 @@
-export const exitProgram = (args) => {
+import { messages } from "../../enums/messages.mjs";
+import { cliArgsValidator } from "../../validators/cliArgsValidator.mjs";
 
+const argsCount = 0;
+
+export const exitProgram = async (args) => {
+  try {
+    if (cliArgsValidator(args, argsCount)){
+      process.emit('.exit');
+    } else {
+      return messages.invalid().concat(EOL);
+    }
+  
+    return '';
+  } catch (error) {
+    console.log(error);
+  }
+  
 }
