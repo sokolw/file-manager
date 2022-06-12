e8b47614528b018f2cd8aeab0fc5cde543ff9660
sokolw <sokolw21@gmail.com>
2022-06-12 17:58:42 +0300

    fix: paths to root and del unuse functions

--- src/action-functions/system/paths.mjs
+++ src/action-functions/system/paths.mjs
@@ -1,16 +1,14 @@
-import { opendir, access } from 'fs/promises';
-import { constants } from 'fs';
-import os, { EOL } from 'os';
+import { opendir } from 'fs/promises';
+import os from 'os';
 import path from 'path';
 import { Application } from './../../app.js';
 
 const getDefaultPath = () => {
   return os.homedir();
-}
+};
 
 const isExistDir = async (rawPath) => {
-  const pathToDir = path.isAbsolute(rawPath) ? 
-    rawPath : path.join(Application._currentPath, rawPath);
+  const pathToDir = createPath(rawPath);
 
   try {
     const directory = await opendir(pathToDir);
@@ -20,32 +18,15 @@ const isExistDir = async (rawPath) => {
   }
 
   return pathToDir;
-}
-
-const isExistPathToFile = async (rawPath) => {
-  const pathToDir = await isExistDir(rawPath);
-  if (pathToDir !== null) {
-    return null;
-  }
-
-  const pathToFile = path.isAbsolute(rawPath) ? 
-    rawPath : path.join(Application._currentPath, rawPath);
-
-  try {
-    await access(pathToFile, constants.F_OK);
-    console.log('can access ' + pathToFile);
-  } catch {
-    console.log('cannot access '+ pathToFile);
-    return null;
-  }
-
-  return pathToFile;
-}
+};
 
 const createPath = (rawPath) => {
-  return path.isAbsolute(rawPath) ? 
-  rawPath : path.join(Application._currentPath, rawPath);
-}
+  const toRoot = '/..';
+  return path.isAbsolute(rawPath) ?
+  (rawPath.startsWith(toRoot) ? rawPath.replace(toRoot, path.parse(Application._currentPath).root) : rawPath)
+  :
+  path.join(Application._currentPath, rawPath);
+};
 
 const parsePaths = (args) => {
   const space = ' ';
@@ -91,12 +72,11 @@ const parsePaths = (args) => {
   }
 
   return storagePaths.parsedPaths;
-}
+};
 
 export {
   getDefaultPath,
   isExistDir,
-  isExistPathToFile,
   createPath,
   parsePaths
 }
