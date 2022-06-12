319b447579c5cecd356aed2bdd60fdd157f74cd7
sokolw <sokolw21@gmail.com>
2022-06-12 17:47:53 +0300

    fix: add parser to changeDirectory

--- src/action-functions/nwd/changeDirectory.mjs
+++ src/action-functions/nwd/changeDirectory.mjs
@@ -1,16 +1,16 @@
-import { messages } from "../../enums/messages.mjs";
-import { cliArgsValidator } from "../../validators/cliArgsValidator.mjs"
+import { messages } from '../../enums/messages.mjs';
+import { cliArgsValidator } from '../../validators/cliArgsValidator.mjs'
 import { EOL } from 'os';
-import { isExistDir } from "../system/paths.mjs";
-import { Application } from "../../app.js";
+import { isExistDir, parsePaths } from '../system/paths.mjs';
+import { Application } from '../../app.js';
 
 const argsCount = 1;
 
 export const changeDirectory = async (args) => {
-  const space = ' ';
-  const spacedPath = [args.join(space)];
-  if (cliArgsValidator(spacedPath, argsCount)){
-    const pathToDir = await isExistDir(...spacedPath);
+  const argsParsed = parsePaths(args);
+
+  if (cliArgsValidator(argsParsed, argsCount)){
+    const pathToDir = await isExistDir(argsParsed[0]);
 
     if (pathToDir !== null) {
       Application._currentPath = pathToDir;
@@ -18,7 +18,9 @@ export const changeDirectory = async (args) => {
       return messages.fail().concat(EOL);
     }
 
+  } else {
+    return messages.invalid().concat(EOL);
   }
 
   return '';
-}
+};
