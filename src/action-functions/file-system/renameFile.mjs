84960e178d7738540b7415eaa389124935778981
sokolw <sokolw21@gmail.com>
2022-06-12 16:42:48 +0300

    fix: add parse to renameFile

--- src/action-functions/file-system/renameFile.mjs
+++ src/action-functions/file-system/renameFile.mjs
@@ -1,27 +1,36 @@
-import { createPath, isExistDir } from "../system/paths.mjs";
-import path from 'path';
-import { rename } from "fs/promises";
-import { messages } from "../../enums/messages.mjs";
+import { createPath, isExistDir, parsePaths } from '../system/paths.mjs';
+import { rename } from 'fs/promises';
+import { messages } from '../../enums/messages.mjs';
 import { EOL } from 'os';
-import { cliArgsValidator } from "../../validators/cliArgsValidator.mjs";
+import { cliArgsValidator } from '../../validators/cliArgsValidator.mjs';
 
 const argsCount = 2;
 
 export const renameFile = async (args) => {
-  if (cliArgsValidator(args, argsCount)){
-    const oldFileName = createPath(args[0]);
-    const newFileName = createPath(args[1]);
+  const argsParsed = parsePaths(args);
+  
+  if (cliArgsValidator(argsParsed, argsCount)){
+    const workPaths = {
+      oldFileName : createPath(argsParsed[0]),
+      newFileName : createPath(argsParsed[1]),
+    };
+
     try {
-      if ((await isExistDir(oldFileName)) !== null || (await isExistDir(newFileName)) !== null) {
-        throw new Error();
+      if ((await isExistDir(workPaths.oldFileName)) !== null || 
+          (await isExistDir(workPaths.newFileName)) !== null
+      ) {
+        throw new Error('It is not file!');
       }
-      await rename(oldFileName, newFileName);
+
+      await rename(workPaths.oldFileName, workPaths.newFileName);
+
     } catch {
       return messages.fail().concat(EOL);
     }
+
   } else {
     return messages.invalid().concat(EOL);
   }
 
   return '';
-}
+};
