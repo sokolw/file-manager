5af85e37607720b9840f56a3bc957481995f448c
sokolw <sokolw21@gmail.com>
2022-06-12 16:27:15 +0300

    fix: add parser

--- src/action-functions/file-system/readFile.mjs
+++ src/action-functions/file-system/readFile.mjs
@@ -1,37 +1,43 @@
-import { createPath } from "../system/paths.mjs";
-import path from 'path';
-import { open } from "fs/promises";
-import { messages } from "../../enums/messages.mjs";
+import { createPath, parsePaths } from '../system/paths.mjs';
+import { open } from 'fs/promises';
+import { messages } from '../../enums/messages.mjs';
 import { EOL } from 'os';
-import { InputManager } from "../../InputManager.js";
+import { InputManager } from '../../InputManager.js';
+import { cliArgsValidator } from '../../validators/cliArgsValidator.mjs';
+
+const argsCount = 1;
 
 export const readFile = async (args) => {
-  const space = ' ';
-  const spacedPath = args.join(space);
-  const pathToFile = createPath(spacedPath);
+  const argsParsed = parsePaths(args);
+  
+  if (cliArgsValidator(argsParsed, argsCount)){
+    const pathToFile = createPath(argsParsed[0]);
 
-  try {
-    const readStream = (await open(pathToFile, 'r'))
-      .createReadStream();
-
-    await new Promise((resolve, reject) => {
-      readStream.on('data', (data) => {
-        InputManager._workingManager.push(data);
+    try {
+      const readStream = (await open(pathToFile, 'r'))
+        .createReadStream();
+  
+      await new Promise((resolve, reject) => {
+        readStream.on('data', (data) => {
+          InputManager._workingManager.push(data);
+        });
+  
+        readStream.on('end', () => {
+          InputManager._workingManager.push(EOL);
+          resolve('OK');
+        });
+  
+        readStream.on('error', () => {
+          reject('FAIL');
+        });
+  
       });
-
-      readStream.on('end', () => {
-        InputManager._workingManager.push(EOL);
-        resolve('OK');
-      });
-
-      readStream.on('error', () => {
-        reject('FAIL');
-      });
-
-    });
-  } catch {
-    return messages.fail().concat(EOL);
+    } catch {
+      return messages.fail().concat(EOL);
+    }
+  } else {
+    return messages.invalid().concat(EOL);
   }
 
   return '';
-}
+};
