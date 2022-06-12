f6d192463f625aa5e751a6c93c3dcaad1ea32bf6
sokolw <sokolw21@gmail.com>
2022-06-12 13:34:51 +0300

    feat: add check cpus for unsupport systems

--- src/action-functions/system/getSystemInfo.mjs
+++ src/action-functions/system/getSystemInfo.mjs
@@ -7,18 +7,19 @@ const argsCount = 1;
 
 export const getSystemInfo = async (args) => {
   const info = {};
+
   try {
     if (cliArgsValidator(args, argsCount, systemArgs)){
       info.result = systemArgs[args[0]]();
     } else {
       return messages.invalid().concat(EOL);
     }
-  
-    return info.result.toString().concat(EOL);
 
-  } catch (error) {
-    console.log(error);
+  } catch {
+    return messages.fail().concat(EOL);
   }
+
+  return info.result.toString().concat(EOL);
 };
 
 const getEol = () => {
@@ -27,25 +28,36 @@ const getEol = () => {
 
 const getCpus = () => {
   const currentCpus = os.cpus();
-  const formattedList = currentCpus.map((item, index) => {
-    const parseNameCpu = (() => { 
-      if (item.model.includes('@')) {
-        const pos = item.model.lastIndexOf('@');
-        return item.model.slice(0, pos);
-      } else {
-        return item.model;
-      }
-    })();
-
-    const cpuSpeedGhz = (() => { 
-      const delimiter = 1000;
-        return `${(item.speed / delimiter).toFixed(2)} GHz`;
-    })();
-
-    return `${index + 1}. Model: ${parseNameCpu}${EOL}   Speed: ${cpuSpeedGhz}`;
-  });
+  const architectureDevSystem = 'x64';
+  const currentArch = os.arch();
+  const formattedList = {};
+  if (currentArch === architectureDevSystem) {
+    formattedList.result = currentCpus.map((item, index) => {
+      const parseNameCpu = (() => { 
+        if (item.model.includes('@')) {
+          const pos = item.model.lastIndexOf('@');
+          return item.model.slice(0, pos);
+        } else {
+          return item.model;
+        }
+      })();
   
-  return `Count CPUs : ${currentCpus.length}${EOL}${formattedList.join(EOL)}`;
+      const cpuSpeedGhz = (() => { 
+        const delimiter = 1000;
+          return `${(item.speed / delimiter).toFixed(2)} GHz`;
+      })();
+  
+      return `${index + 1}. Model: ${parseNameCpu}${EOL}   Speed: ${cpuSpeedGhz}`;
+    });
+  } else {
+    const warnMessage = `Unsupported system: ${currentArch}, to display in GHz`;
+    formattedList.result = currentCpus.map((item, index) => {  
+      return `${index + 1}. Model: ${item.model}${EOL}   Speed: ${item.speed}`;
+    });
+    formattedList.result.unshift(warnMessage);
+  }
+
+  return `Count CPUs : ${currentCpus.length}${EOL}${formattedList.result.join(EOL)}`;
 };
 
 const getHomedir = () => {
